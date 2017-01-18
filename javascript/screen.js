d7f54fc5c8e9c67f06e49ab65cd68660c15a8d41
niuben <741001695@qq.com>
2017-01-18 12:12:27 +0800

    Create screen.js

---
+++ javascript/screen.js
@@ -0,0 +1,31 @@
+/* ---- */
+// 得到页面的高度 
+function getHeight() {
+  if (document.documentElement.scrollHeight < document.documentElement.clientHeight) {
+    if ("\v"=="v") {
+      return document.compatMode == "CSS1Compat" ? document.documentElement.clientHeight : document.body.clientHeight;
+    } else {
+      return self.innerHeight;
+    }
+  } else {
+    return document.documentElement.scrollHeight;
+  } 
+};
+/* ---- */
+getHeight();
+
+/* ---- */
+// 得到页面宽度
+function getWidth() {
+   if (document.documentElement.scrollWidth < document.documentElement.clientWidth) {
+        if ("\v" == "v") {
+            return document.compatMode == "CSS1Compat" ? document.documentElement.clientWidth : document.body.clientWidth;
+        } else {
+            return self.innerWidth;
+        }
+    }else {
+      return document.documentElement.scrollWidth;
+    }
+} 
+/* ---- */
+getWidth();
