b8acb01d5183796e0e1132dba8b8ddcbc44a845c
niuben <741001695@qq.com>
2017-01-18 12:15:16 +0800

    Create number.js

---
+++ javascript/number.js
@@ -0,0 +1,23 @@
+/* ---- */
+// 判断是否是帧数
+function isInt(num) {    
+    if (Math.floor(num) == this) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/* ---- */
+isInt(num)
+
+/* ---- */
+// 判断是否是小数
+function isFloat(num) {
+    if (Math.floor(num) != this) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/* ---- */
+isFloat(num)
