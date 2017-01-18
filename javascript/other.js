647ae230df246c400a61e158e75a2645fa8bad74
niuben <741001695@qq.com>
2017-01-18 13:06:23 +0800

    其他常用的操作

---
+++ javascript/other.js
@@ -0,0 +1,15 @@
+/* ---- */
+//隐藏四位手机号
+function hideTelephone(telephone) {
+		if( !telephone ) {
+			return "";
+		}
+
+		if( telephone.length != 11){
+			return telephone;
+		}
+
+		return telephone.substr(0, 4) + "****" + telephone.substr(8, 11);
+}
+/* ---- */
+hideTelephone("13003334444);
