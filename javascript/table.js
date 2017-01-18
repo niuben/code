f54715f740c87f473837d9a17335855f59f01d1c
niuben <741001695@qq.com>
2017-01-18 13:07:30 +0800

    Update table.js

--- javascript/table.js
+++ javascript/table.js
@@ -1,1 +1,9 @@
-
+//折叠表格中单独一行.js
+function slideUp(trEle, callback) {
+	setTimeout(function(){
+		$(trEle).find("td").hide();		
+	}, 120);	
+	$(trEle).slideUp("fast", function(){
+		callback && callback();
+	});
+}
