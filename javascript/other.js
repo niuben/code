c9429e335fdf5109f3b90700706aba6c2a329746
niuben <741001695@qq.com>
2017-02-14 15:07:58 +0800

    Update other.js

--- javascript/other.js
+++ javascript/other.js
@@ -18,24 +18,46 @@ hideTelephone("13003334444");
 *  callback: [function] 请求成功的回调函数 
 *  errCallback: [function] 可选 请求失败的回调函数
 */
-function loadscript(url, callback, errCallback){
+function(url, callback, errCallback){
+    
+    //判断是否已经加载
+    var scriptEles = document.getElementsByTagName("script");
+    for(var i = 0; i < scriptEles.length; i++) {
+        if(scriptEles[i].src && scriptEles[i].src != url) {            
+            continue;
+        }
+        var status = scriptEles[i].getAttribute("_status");
+        if(status == "loaded"){
+            callback(); 
+        }else if(status == "error"){
+            errCallback && errCallback();
+        }
+        return;
+    }
+ 
     var script = document.createElement("script");
-    script.type = "text/javascript";
+    script.type = "text/javascript";        
     if (script.readyState){ //IE
         script.onreadystatechange = function(){
             if (script.readyState == "loaded"){
             	script.onreadystatechange = null;
-            	errCallback && errCallback();	
+                script.setAttribute("_status", "error");
+
+            	errCallback && errCallback();
             }else if(script.readyState == "complete"){
                 script.onreadystatechange = null;
+                script.setAttribute("_status", "loaded");
+
                 callback();
             }
         };
     }else{ //Others
         script.onload = function(){
+            script.setAttribute("_status", "loaded");            
             callback();
         };
         script.onerror = function(){
+            script.setAttribute("_status", "error");            
         	errCallback && errCallback();
         };
     }
@@ -43,6 +65,7 @@ function loadscript(url, callback, errCallback){
     script.src = url;
     document.getElementsByTagName("head")[0].appendChild(script);
 }
+
 /* ---- */
 LoadScript("url", function(){	
      alert("success");	
