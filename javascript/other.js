d207674125e913653c6d191d5451e4bb2c4cfdef
niuben <741001695@qq.com>
2017-02-24 12:46:37 +0800

    Update other.js

--- javascript/other.js
+++ javascript/other.js
@@ -14,16 +14,40 @@ function hideTelephone(telephone) {
 hideTelephone("13003334444");
 /* ---- */
 /*
-*  url: [string] 远程第三方js文件地址
+*  url: [string | array ] 远程第三方js文件地址
 *  callback: [function] 请求成功的回调函数 
 *  errCallback: [function] 可选 请求失败的回调函数
 */
-function(url, callback, errCallback){
+
+/*
+*  在数组中查找特定的值；
+*  arr: [arr] 数组
+*  val: 查找值
+*/
+function searchArr(arr, val) {
+    var isHave = 0;
+    for(var i = 0; i < arr.length - 1; i++){
+        if(arr[i] == val){
+            return true;
+        }
+    }
+    return false;
+}
+var loadedNum;
+
+
+module.exports = function(url, callback, errCallback){
     
+    loadedNum = 0;
+
+    //判断url是字符串还是url
+    var urlArr = typeof url == "string" ? [url] : url;
+
     //判断是否已经加载
     var scriptEles = document.getElementsByTagName("script");
     for(var i = 0; i < scriptEles.length; i++) {
-        if(scriptEles[i].src && scriptEles[i].src != url) {            
+        var src = scriptEles[i].src;
+        if( src && searchArr(url) == false) {
             continue;
         }
         var status = scriptEles[i].getAttribute("_status");
@@ -34,38 +58,44 @@ function(url, callback, errCallback){
         }
         return;
     }
- 
-    var script = document.createElement("script");
-    script.type = "text/javascript";        
-    if (script.readyState){ //IE
-        script.onreadystatechange = function(){
-            if (script.readyState == "loaded"){
-            	script.onreadystatechange = null;
-                script.setAttribute("_status", "error");
+    
+    for(var n = 0; n < urlArr.length; n ++) {
 
-            	errCallback && errCallback();
-            }else if(script.readyState == "complete"){
-                script.onreadystatechange = null;
-                script.setAttribute("_status", "loaded");
+        var script = document.createElement("script");
+        script.type = "text/javascript";        
+        if (script.readyState){ //IE
+            script.onreadystatechange = function(){
+                loadedNum++;
+                
+                if (script.readyState == "loaded"){
+                	script.onreadystatechange = null;
+                    script.setAttribute("_status", "error");
 
-                callback();
-            }
-        };
-    }else{ //Others
-        script.onload = function(){
-            script.setAttribute("_status", "loaded");            
-            callback();
-        };
-        script.onerror = function(){
-            script.setAttribute("_status", "error");            
-        	errCallback && errCallback();
-        };
+                	loadedNum == urlArr.length && errCallback && errCallback();
+                }else if(script.readyState == "complete"){
+                    script.onreadystatechange = null;
+                    script.setAttribute("_status", "loaded");
+
+                    loadedNum == urlArr.length && callback();
+                }
+            };
+        }else{ //Others
+            script.onload = function(){
+                loadedNum++;
+                script.setAttribute("_status", "loaded");            
+                loadedNum == urlArr.length && callback();
+            };
+            script.onerror = function(){
+                loadedNum++;
+                script.setAttribute("_status", "error");            
+            	loadedNum == urlArr.length && errCallback && errCallback();
+            };
+        }
+
+        script.src = url[n];
+        document.getElementsByTagName("head")[0].appendChild(script);
     }
-
-    script.src = url;
-    document.getElementsByTagName("head")[0].appendChild(script);
 }
-
 /* ---- */
 LoadScript("url", function(){	
      alert("success");	
